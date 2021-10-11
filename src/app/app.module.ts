54409e7924cb8c67226ba8ac2bf6ee08c7be9aae
Wanderson <wandersonsales.dev@gmail.com>
2021-10-11 17:51:20 -0300

    :sparkles: Footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
